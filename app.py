03ff339e6d272a7a80b0884b8439baa82cee4b36
EndahGitaCahyani07 <148411705+EndahGitaCahyani07@users.noreply.github.com>
2024-05-25 22:55:37 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# import streamlit as st
+# from streamlit_option_menu import option_menu
+
+# with st.sidebar:
+#     selected = option_menu(
+#         'Air Pollution Conditions and Prediction',
+#          [
+#              'Visualization',
+#              'AQI Category Prediction'
+#          ],
+#         menu_icon='cast',
+#         icons=['kanban', 'activity'],
+#         default_index=0
+# )
